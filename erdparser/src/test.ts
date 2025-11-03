2f73e723b363101b7903280f7f09bd1cd4d69469
brunolzrosa <brunoluiszrosa@gmail.com>
2025-11-03 01:08:01 -0300

    generated all tests again to check if they pass

---
+++ erdparser/src/test.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+import { Diagram } from "./interfaces";
+import readErdplusFile from "./readErdplusFile";
+import parseErdToRelational from "./parser";
+import saveErdPlusFile from "./saveErdplusFile";
+
+const inputFolder = 'data/tests'
+const outputFolder = 'data/tests/results';
+
+(async () => {
+  try {
+
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdir(outputFolder, { recursive: true }, (err) => {
+            if (err) throw err;
+        });
+    }
+
+    const files = fs.readdirSync(inputFolder).filter(file => file.endsWith(".erdplus"));
+
+    for (const fileName of files) {
+        const baseName = path.basename(fileName, ".erdplus");
+        try {
+            const dg: Diagram = await readErdplusFile(path.join(inputFolder, fileName));
+            const rel: Diagram = parseErdToRelational(dg)
+            const outputPath = path.join(outputFolder, `${baseName}_rel.erdplus`);
+            saveErdPlusFile(rel, outputPath);
+        } catch (error) {
+            console.error("Error parsing file:", error)
+        }
+    }
+
+    
+  } catch (error) {
+    console.error("Error parsing files:", error);
+  }
+})();
--- toTest.md
+++ toTest.md
@@ -1,20 +1,20 @@
-1. [V] Entidade-Atributo
+1. [ ] Entidade-Atributo
 
-2. [V] Entidade-Relacionamento
+2. [ ] Entidade-Relacionamento
 
-3. [V] Cascata de atributos
+3. [ ] Cascata de atributos
 
-4. [V] Relacionamento recursivo
+4. [ ] Relacionamento recursivo
 
-5. [V] N-Ário: Pelo novo formato do erdplus, não existe mais relacionamento n-ário própriamente dito, então eu não precio sequer me preocupar com isso (mas acho que é válido dizer isso)
+5. [ ] N-Ário: Pelo novo formato do erdplus, não existe mais relacionamento n-ário própriamente dito, então eu não precio sequer me preocupar com isso (mas acho que é válido dizer isso)
 
-6. [V] Entidades fracas + (relacionamentos identifying + cascata)
+6. [ ] Entidades fracas + (relacionamentos identifying + cascata)
 
-7. [V] Atributos de relacionamentos: Só funciona relacionamentos m:n.
+7. [ ] Atributos de relacionamentos: Só funciona relacionamentos m:n.
 
-8. [V] Supertipos e herança de herança
+8. [ ] Supertipos e herança de herança
 
-9. [V] Relacionamento Supertipo
+9. [ ] Relacionamento Supertipo
 
 10. [ ] Multivalorado Entidade Fraca
 
